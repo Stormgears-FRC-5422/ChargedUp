bce73a05ce5cbe717aa24efe62c13b6f79a7bb0a
TT-7 <tanush.tumula7@gmail.com>
2023-02-04 13:03:40 -0500

    Work on neopixels, make them blink.

--- src/main/java/frc/robot/subsystems/NeoPixel.java
+++ src/main/java/frc/robot/subsystems/NeoPixel.java
@@ -10,21 +10,34 @@ import frc.utils.lights.AddressableLEDBufferRGBW;
 import static frc.robot.Constants.*;
 public class NeoPixel extends SubsystemBase {
     private final AddressableLED lightStrip = new AddressableLED(0);
-    private final AddressableLEDBuffer buffer = new AddressableLEDBuffer(12);
+    private final AddressableLEDBufferRGBW buffer = new AddressableLEDBufferRGBW(14);
     private Color8Bit fullColor;
+    private int rainbowHue = 0;
+
+    private int countLED = 0;
+
 
     public NeoPixel() {
-        lightStrip.setLength(buffer.getLength());
+        lightStrip.setLength(buffer.getEffectiveLength());
         lightStrip.setData(buffer);
         lightStrip.start();
-
-        fullColor = new Color8Bit(64, 64, 0);
+        fullColor = new Color8Bit(153, 51, 255);
+//        fullColor = new Color8Bit(255, 213, 0); yellow
     }
 
     @Override
     public void periodic() {
-        setAll(fullColor);
         lightStrip.setData(buffer);
+    //    rainbow();
+        countLED++;
+        System.out.println(countLED);
+        if (countLED % 2 == 0) {
+            setAll(fullColor);
+            countLED = 0;
+        } else{
+            setAll(null);
+        }
+
 
     }
 
@@ -32,7 +45,17 @@ public class NeoPixel extends SubsystemBase {
         if (color != null) {
             for (int i = 0; i < buffer.getLength(); i++) {
                 buffer.setLED(i, color);
+
             }
         }
+    }
+
+    private void rainbow() {
+        for (int i = 0; i < buffer.getLength(); i++) {
+            final int hue = (rainbowHue + (i * 180 / buffer.getLength())) % 180;
+            buffer.setHSV(i, hue, 255, 128);
+        }
+        rainbowHue += 3;
+        rainbowHue %= 180;
     }
 }
