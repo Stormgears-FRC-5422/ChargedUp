80beb043e5f92b2ccc2d915c320910e8835079c9
TT-7 <tanush.tumula7@gmail.com>
2023-02-05 14:05:48 -0500

    Blinking lights worked upon

--- src/main/java/frc/robot/subsystems/NeoPixel.java
+++ src/main/java/frc/robot/subsystems/NeoPixel.java
@@ -11,45 +11,47 @@ import static frc.robot.Constants.*;
 public class NeoPixel extends SubsystemBase {
     private final AddressableLED lightStrip = new AddressableLED(0);
     private final AddressableLEDBufferRGBW buffer = new AddressableLEDBufferRGBW(14);
-    private Color8Bit fullColor;
+    private final Color8Bit fullColor;
+    private final Color8Bit blankColor;
     private int rainbowHue = 0;
-
-    private int countLED = 0;
-
+    public int countLED = 0;
 
     public NeoPixel() {
         lightStrip.setLength(buffer.getEffectiveLength());
         lightStrip.setData(buffer);
         lightStrip.start();
-        fullColor = new Color8Bit(153, 51, 255);
-//        fullColor = new Color8Bit(255, 213, 0); yellow
+        fullColor = new Color8Bit(153, 51, 255); //purple
+        blankColor = new Color8Bit(0, 0, 0); //blank or off
+
+    }
+    public void blink(){
+        countLED++;
+        if (countLED % 50 > 25) {
+            setAll(blankColor);
+            System.out.println("should be off");
+        };
+        if (countLED % 50 < 25){
+            setAll(fullColor);
+        };
+        if (countLED == 50) {
+            countLED = 0;
+        };
     }
 
     @Override
     public void periodic() {
         lightStrip.setData(buffer);
-    //    rainbow();
-        countLED++;
+        //rainbow(); - rainbow light called
+        blink(); //toggles between colors, currently full and blank
         System.out.println(countLED);
-        if (countLED % 2 == 0) {
-            setAll(fullColor);
-            countLED = 0;
-        } else{
-            setAll(null);
-        }
-
-
     }
-
     private void setAll(Color8Bit color) {
         if (color != null) {
             for (int i = 0; i < buffer.getLength(); i++) {
                 buffer.setLED(i, color);
-
             }
         }
     }
-
     private void rainbow() {
         for (int i = 0; i < buffer.getLength(); i++) {
             final int hue = (rainbowHue + (i * 180 / buffer.getLength())) % 180;
