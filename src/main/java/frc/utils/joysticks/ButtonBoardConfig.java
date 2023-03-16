b291e5abc6f85132682f79e038584de96adf0490
Ajay Krishnan <krishnanajay345@gmail.com>
2023-03-15 21:08:15 -0400

    placeGamePiece Command but still error when binding it in ButtonBoardConfig

--- src/main/java/frc/utils/joysticks/ButtonBoardConfig.java
+++ src/main/java/frc/utils/joysticks/ButtonBoardConfig.java
@@ -3,18 +3,34 @@ package frc.utils.joysticks;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.NeoPixel;
+import frc.robot.commands.autoScoring.placeGamePiece;
 
 public class ButtonBoardConfig {
 
   ButtonBoard m_buttonboard1;
   ButtonBoard m_buttonboard2;
 
+  enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
+  }
+
+  enum gamePiece {
+    CONE,
+    CUBE
+  }
+
+  Level m_Level;
+
+  gamePiece m_gamePiece;
+
   Exception killSwitch;
 
   NeoPixel neoPixel;
 
-  private int grid1, grid2, grid3, grid4, grid5, grid6, grid7, grid8, grid9;
-  int[] gridState = {grid1, grid2, grid3, grid4, grid5, grid6, grid7, grid8, grid9};
+  private int grid;
+
 
   public ButtonBoardConfig(NeoPixel neoPixel) {
     m_buttonboard1 = new ButtonBoard(1);
@@ -42,6 +58,7 @@ public class ButtonBoardConfig {
 
 
 
+<<<<<<< Updated upstream
 //    new Trigger(m_buttonboard2::grid1).onTrue(new InstantCommand(() -> grid1 = 1));
 //    new Trigger(m_buttonboard2::grid2).onTrue(new InstantCommand(() -> grid2 = 2));
 //    new Trigger(m_buttonboard2::grid3).onTrue(new InstantCommand(() -> grid3 = 3));
@@ -51,6 +68,31 @@ public class ButtonBoardConfig {
 //    new Trigger(m_buttonboard2::grid7).onTrue(new InstantCommand(() -> grid7 = 7));
 //    new Trigger(m_buttonboard2::grid8).onTrue(new InstantCommand(() -> grid8 = 8));
 //    new Trigger(m_buttonboard2::grid9).onTrue(new InstantCommand(() -> grid9 = 9));
+=======
+
+//    if (m_buttonboard4.kill()) {
+//      System.out.println("Killing robot");
+//      throw new KillSwitch("Killing robot");
+//
+//    }
+
+    new Trigger(m_buttonboard1::leftSub).onTrue(new InstantCommand(() -> System.out.println("Left Sub Selected")));
+    new Trigger(m_buttonboard1::rightSub).onTrue(new InstantCommand(() -> System.out.println("Right Sub Selected")));
+    new Trigger(m_buttonboard2::grid1).onTrue(new InstantCommand(() -> grid = 1));
+    new Trigger(m_buttonboard2::grid2).onTrue(new InstantCommand(() -> grid = 2));
+    new Trigger(m_buttonboard2::grid3).onTrue(new InstantCommand(() -> grid = 3));
+    new Trigger(m_buttonboard2::grid4).onTrue(new InstantCommand(() -> grid = 4));
+    new Trigger(m_buttonboard2::grid5).onTrue(new InstantCommand(() -> grid = 5));
+    new Trigger(m_buttonboard2::grid6).onTrue(new InstantCommand(() -> grid = 6));
+    new Trigger(m_buttonboard2::grid7).onTrue(new InstantCommand(() -> grid = 7));
+    new Trigger(m_buttonboard2::grid8).onTrue(new InstantCommand(() -> grid = 8));
+    new Trigger(m_buttonboard2::grid9).onTrue(new InstantCommand(() -> grid = 9));
+    new Trigger(m_buttonboard1::High).onTrue(new InstantCommand(() -> m_Level = Level.HIGH));
+    new Trigger(m_buttonboard1::Medium).onTrue(new InstantCommand(() -> m_Level = Level.MEDIUM));
+    new Trigger(m_buttonboard1::Low).onTrue(new InstantCommand(() -> m_Level = Level.LOW));
+    new Trigger(m_buttonboard1::Cone).onTrue(new InstantCommand(() -> m_gamePiece = gamePiece.CONE));
+    new Trigger(m_buttonboard1::Cone).onTrue(new InstantCommand(() -> m_gamePiece = gamePiece.CUBE));
+>>>>>>> Stashed changes
     new Trigger(m_buttonboard1::kill).onTrue(new InstantCommand(() -> System.exit(0)));
     new Trigger(m_buttonboard1::floor).onTrue(new InstantCommand(() -> System.out.println("Floor Selected"))).and( new Trigger(m_buttonboard1::cubeCone).onTrue(new InstantCommand(() -> neoPixel.setAllColor( NeoPixel.PURPLE_COLOR)))).or(new Trigger(m_buttonboard1::cubeCone).onFalse(new InstantCommand(() -> neoPixel.setAllColor( NeoPixel.YELLOW_COLOR))););
     new Trigger(m_buttonboard1::leftSub).onTrue(new InstantCommand(() -> System.out.println("Left Sub Selected"))).and( new Trigger(m_buttonboard1::cubeCone).onTrue(new InstantCommand(() -> neoPixel.setAllColor( NeoPixel.PURPLE_COLOR)))).or(new Trigger(m_buttonboard1::cubeCone).onFalse(new InstantCommand(() -> neoPixel.setAllColor( NeoPixel.YELLOW_COLOR))););
@@ -117,6 +159,7 @@ public class ButtonBoardConfig {
 //              gridState[i] = 0;
 //            }
 //    }
+<<<<<<< Updated upstream
 //
 //
 //
@@ -133,5 +176,30 @@ public class ButtonBoardConfig {
 //    new Trigger(m_buttonboard1::cancel).onTrue(new InstantCommand(() -> System.out.println("Cancel")));
 //
 //  }
+=======
+
+    if (grid != 0) {
+        new InstantCommand(() -> new placeGamePiece(m_Level, m_gamePiece));
+      grid = 0;
+    } else {
+        new InstantCommand(() -> System.out.println("No Grid Running"));
+    }
+
+    new Trigger(m_buttonboard1::cancel).onTrue(new InstantCommand(() -> { grid = 0;}));
+
+    /*
+    new Trigger(m_buttonboard2::grid1).onTrue(new InstantCommand(() -> System.out.println("Grid1 Selected")));
+    new Trigger(m_buttonboard2::grid2).onTrue(new InstantCommand(() -> System.out.println("Grid2 Selected")));
+    new Trigger(m_buttonboard2::grid3).onTrue(new InstantCommand(() -> System.out.println("Grid3 Selected")));
+    new Trigger(m_buttonboard2::grid4).onTrue(new InstantCommand(() -> System.out.println("Grid4 Selected")));
+    new Trigger(m_buttonboard2::grid5).onTrue(new InstantCommand(() -> System.out.println("Grid5 Selected")));
+    new Trigger(m_buttonboard2::grid6).onTrue(new InstantCommand(() -> System.out.println("Grid6 Selected")));
+    new Trigger(m_buttonboard2::grid7).onTrue(new InstantCommand(() -> System.out.println("Grid7 Selected")));
+    new Trigger(m_buttonboard2::grid8).onTrue(new InstantCommand(() -> System.out.println("Grid8 Selected")));
+    new Trigger(m_buttonboard2::grid9).onTrue(new InstantCommand(() -> System.out.println("Grid9 Selected")));
+    new Trigger(m_buttonboard1::confirm).onTrue(new InstantCommand(() -> System.out.println("Confirm")));
+    new Trigger(m_buttonboard1::cancel).onTrue(new InstantCommand(() -> System.out.println("Cancel")));
+     */
+>>>>>>> Stashed changes
   }
 }
