5c2245ebdfea97d91543fcf5161ad71da56045dc
Michelle <ojemsotelo@gmail.com>
2023-03-26 13:26:35 -0400

    Update Y-control

--- src/main/java/frc/robot/commands/auto/autoManeuvers/ArmToPickUp.java
+++ src/main/java/frc/robot/commands/auto/autoManeuvers/ArmToPickUp.java
@@ -1,29 +1,29 @@
 package frc.robot.commands.auto.autoManeuvers;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotState;
+import frc.robot.constants.Constants;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.stormnet.StormNet;
 
 import static frc.robot.constants.Constants.kXYArmManualSpeed;
 
 public class ArmToPickUp extends CommandBase {
-
   Arm arm;
-
   StormNet stormNet;
-  Pose2d gripperPose;
-  double yPos;
 
-  private boolean commandCondition;
-  private static double tolerance = 0.05;
+  Pose2d gripperPose;
+  double yTarget;
+  double dx;
+  double dy;
+  boolean closeToTarget;
 
   public ArmToPickUp(Arm arm, StormNet stormNet){
     this.arm = arm;
     this.stormNet = stormNet;
+    this.addRequirements(arm);
   }
 
   @Override
@@ -31,41 +31,45 @@ public class ArmToPickUp extends CommandBase {
     System.out.println("ArmToPickUp command running");
 
     gripperPose = arm.getGripperPose();
-    yPos = gripperPose.getY();
+    yTarget = Constants.ArmConstants.pickDoubleSubstation.getY();
+    dx = 0;
+    dy = 0;
   }
 
   @Override
   public void execute() {
-    Translation2d gripper = arm.getGripperPose().getTranslation();
+    gripperPose = arm.getGripperPose();
     double distance = stormNet.getLidarDistance();
-    double dx = 0;
-    double dy = 0;
     var currentRange = RobotState.getInstance().getLidarRange();
 
-//    if (distance <= currentRange.getMin()) {
-//      dx = -kXYArmManualSpeed;
-//    } else if (distance >= currentRange.getMax() && distance < 0.5) {
-//      dx = kXYArmManualSpeed;
-//    }
-    if (distance <= 0.5)
-      dx = Math.signum(distance - currentRange.getCenter()) * kXYArmManualSpeed;
+    closeToTarget = false;
+    if (distance <= currentRange.getMin()) { // Too close to target
+      dx = -kXYArmManualSpeed;
+    } else if (distance >= currentRange.getMax() && distance < 0.5) {  // Too far from target
+      dx = kXYArmManualSpeed;
+    } else if (distance >= 0.5) { // Way to far from target
+      dx = 0;
+    } else { // close to target
+      dx = 0;
+      closeToTarget = true;
+    }
 
-    double y = gripper.getY();
-    if (y > yPos + 0.015) {
-      dy = -kXYArmManualSpeed;
-    } else if (y < yPos - 0.015) {
+    double y = gripperPose.getY();
+    if (y < yTarget - 0.015 || dy > 0 && y < yTarget) {
       dy = kXYArmManualSpeed;
+    } else if (y > yTarget + 0.015 || dy < 0 && y > yTarget) {
+      dy = -kXYArmManualSpeed;
+    } else {
+      dy = 0;
     }
 
 //    System.out.println("Lidar distance: " + distance + " dx: " + dx + " dy: " + dy + " y: " + y + " yPos: " + yPos);
     arm.xyMoveArm(new ChassisSpeeds(dx, dy, 0));
-    commandCondition = gripper.getX() >= currentRange.getCenter() - tolerance &&
-            gripper.getX() <= currentRange.getCenter() + tolerance;
   }
 
   @Override
   public boolean isFinished() {
-    return commandCondition;
+    return (dx == 0 && dy == 0 && closeToTarget);
   }
 
   @Override
